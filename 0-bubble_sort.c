db6009ae6d2302239de8f72354fdadb94d51d7fe
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-07-25 15:08:38 -0700

    Sorting algorithmn

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,9 +22,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
